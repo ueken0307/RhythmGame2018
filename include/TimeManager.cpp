fa92e4ec60dfb83a6712ea18fb204744bd835801
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-09-13 23:48:49 +0900

    [update]TimeManagerクラスの大まかな実装
    とりあえず

--- include/TimeManager.cpp
+++ include/TimeManager.cpp
@@ -1,1 +1,35 @@
 #include "TimeManager.hpp"
+
+std::chrono::high_resolution_clock::time_point getTime() {
+  return std::chrono::high_resolution_clock::now();
+}
+
+int diff(std::chrono::high_resolution_clock::time_point start,
+  std::chrono::high_resolution_clock::time_point end) {
+  std::chrono::microseconds diff = 
+    std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+  return static_cast<int>(diff.count());
+}
+
+TimeManager::TimeManager() {
+  startTime = getTime();
+  nowTime = getTime();
+}
+
+void TimeManager::start() {
+  startTime = getTime();
+  nowTime = getTime();
+}
+
+void TimeManager::update() {
+  nowTime = getTime();
+}
+
+double TimeManager::nowSeconds() {
+  return diff(startTime, nowTime)*0.000001;
+}
+
+int TimeManager::nowMicroSeconds() {
+  return diff(startTime, nowTime);
+}
--- include/TimeManager.hpp
+++ include/TimeManager.hpp
@@ -1,4 +1,19 @@
 #ifndef TIME_MANAGER_HPP
 #define TIME_MANAGER_HPP
 
+#include <chrono>
+
+class TimeManager {
+public:
+  TimeManager();
+  void start();
+  void update();
+  double nowSeconds();
+  int nowMicroSeconds();
+  
+private:
+  std::chrono::high_resolution_clock::time_point startTime;
+  std::chrono::high_resolution_clock::time_point nowTime;
+};
+
 #endif // TIME_MANAGER_HPP
