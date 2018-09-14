be784eb678fb9d1de4b5ce383dafb0fe03c099cb
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-09-14 12:48:47 +0900

    [update]RhytymManagerの大まかな実装
    （関数だいたい羅列しただけ）

--- include/RhythmManager.cpp
+++ include/RhythmManager.cpp
@@ -1,1 +1,18 @@
 #include "RhythmManager.hpp"
+
+RhythmManager::RhythmManager():offset(0),bmsCount(0) {
+  
+}
+
+void RhythmManager::start() {
+  offset = 0;
+  bmsCount = 0;
+}
+
+void RhythmManager::update() {
+  //calc bmscount
+}
+
+void RhythmManager::setOffset(int offset) {
+  this->offset = offset;
+}
--- include/RhythmManager.hpp
+++ include/RhythmManager.hpp
@@ -1,4 +1,17 @@
 #ifndef RHYTHM_MANAGER_HPP
 #define RHYTHM_MANAGER_HPP
 
+class RhythmManager {
+public:
+  RhythmManager();
+  void start();
+  void update();
+  void setOffset(int);
+  int getBmsCount() const { return bmsCount; };
+
+private:
+  int offset;
+  int bmsCount;
+};
+
 #endif // RHYTHM_MANAGER_HPP
