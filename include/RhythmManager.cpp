8689a3454cba99dd19f4cf528027bc4db740d1f6
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-09-23 18:09:30 +0900

    [update]BPM変化点の秒取得の追加

--- include/RhythmManager.cpp
+++ include/RhythmManager.cpp
@@ -6,11 +6,19 @@ BpmData::BpmData(int count, double bpm) {
   this->bpm = bpm;
 }
 
-RhythmManager::RhythmManager(std::vector<BpmData> &bpmDatas,double offset){
-  for (auto& bpmData : bpmDatas) {
-    this->bpmDatas.push_back(BpmData(bpmData.count, bpmData.bpm));
-    this->changeTimes.push_back((60.0 / bpmData.bpm)*(bpmData.count / 2400.0));
+RhythmManager::RhythmManager(std::vector<BpmData> &bpmDatas,double offset = 0.0){
+
+  //BPM•Ï‰»•b‚ÌŽæ“¾
+  this->bpmDatas.push_back(BpmData(bpmDatas[0].count, bpmDatas[0].bpm));
+  this->changeTimes.push_back(0.0);
+
+  for (int i = 1; i < bpmDatas.size(); ++i) {
+    this->bpmDatas.push_back(BpmData(bpmDatas[i].count, bpmDatas[i].bpm));
+    this->changeTimes.push_back((60.0 / bpmDatas[i-1].bpm)*((bpmDatas[i].count - bpmDatas[i-1].count) / 2400.0) + changeTimes[i-1]);
+
+    printf("%lf\n",changeTimes[i]);
   }
+
 
   this->offset = offset;
   this->bmsCount = 0;
