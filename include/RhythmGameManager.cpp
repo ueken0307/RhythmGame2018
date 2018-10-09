89ccaa793c614ad4c4ce7a429993a8682639e92b
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-10-09 17:03:36 +0900

    [update]Note,RhythmGameManagerクラスをとりあえず実装
    と　り　あ　え　ず

--- include/RhythmGameManager.cpp
+++ include/RhythmGameManager.cpp
@@ -1,1 +1,7 @@
 #include"RhythmGameManager.hpp"
+
+Note::Note(int count,int lane,int length) {
+  this->count = count;
+  this->lane = lane;
+  this->length = length;
+}
--- include/RhythmGameManager.hpp
+++ include/RhythmGameManager.hpp
@@ -1,4 +1,25 @@
 #ifndef RHYTHM_GAME_MANAGER_HPP
 #define RHYTHM_GAME_MANAGER_HPP
 
+#include "RhythmManager.hpp"
+
+class Note {
+public:
+  Note(int, int, int);
+  int count;
+  int lane;
+  int length;
+};
+
+class RhythmGameManager {
+public:
+
+
+private:
+  RhythmManager rhythmManager;
+  std::vector<Note> notes;
+
+
+};
+
 #endif // !RHYTHM_GAME_MANAGER_HPP
