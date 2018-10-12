6ffc5610446b9c4880be309403d0dcb3738dca7e
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-10-12 11:58:06 +0900

    [update]クラス名をNoteからNoteDataに変更
    BpmDataのほうと統一

--- include/RhythmGameManager.cpp
+++ include/RhythmGameManager.cpp
@@ -1,6 +1,6 @@
 #include"RhythmGameManager.hpp"
 
-Note::Note(int count,int lane,int length) {
+NoteData::NoteData(int count,int lane,int length) {
   this->count = count;
   this->lane = lane;
   this->length = length;
