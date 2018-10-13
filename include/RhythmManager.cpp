5bc169800939e0bbfd37f31d6b8beddc5ba754f6
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-10-13 17:20:00 +0900

    [fix]printfの削除
    むだなにゃつ

--- include/RhythmManager.cpp
+++ include/RhythmManager.cpp
@@ -15,8 +15,6 @@ RhythmManager::RhythmManager(std::vector<BpmData> &bpmDatas,double offset = 0.0)
   for (int i = 1; i < bpmDatas.size(); ++i) {
     this->bpmDatas.push_back(BpmData(bpmDatas[i].count, bpmDatas[i].bpm));
     this->changeTimes.push_back((60.0 / bpmDatas[i-1].bpm)*((bpmDatas[i].count - bpmDatas[i-1].count) / 2400.0) + changeTimes[i-1]);
-
-    printf("%lf\n",changeTimes[i]);
   }
 
 
