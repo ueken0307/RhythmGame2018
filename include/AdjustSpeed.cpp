ca2377b9d7d0c92f860e7e7588d9d6a9bfdb949f
Kentarou Ueda <ueken0307@hotmail.co.jp>
2018-11-03 00:04:07 +0900

    [update]AdjustSpeedに遷移するように変更

--- include/AdjustSpeed.cpp
+++ include/AdjustSpeed.cpp
@@ -1,1 +1,29 @@
 #include"AdjustSpeed.hpp"
+
+
+void AdjustSpeed::init() {
+
+}
+
+void AdjustSpeed::update() {
+  if (Input::KeyEnter.clicked) {
+    changeScene(L"Game");
+  }
+
+}
+
+void AdjustSpeed::draw() const{
+
+}
+
+void AdjustSpeed::drawFadeIn(double t) const {
+  draw();
+  Transformer2D tr(Graphics2D::GetTransform().inverse());
+  Window::ClientRect().draw(ColorF(0.0).setAlpha(1.0 - t));
+}
+
+void AdjustSpeed::drawFadeOut(double t) const {
+  draw();
+  Transformer2D tr(Graphics2D::GetTransform().inverse());
+  Window::ClientRect().draw(ColorF(0.0).setAlpha(t));
+}
--- include/AdjustSpeed.hpp
+++ include/AdjustSpeed.hpp
@@ -1,3 +1,20 @@
 #ifndef ADJUST_SPEED_HPP
 
+#include <Siv3D.hpp>
+#include <HamFramework.hpp>
+
+#include"ShareData.hpp"
+
+class AdjustSpeed :public SceneManager<String, ShareData>::Scene {
+public:
+  void init() override;
+  void update() override;
+  void draw() const override;
+  void drawFadeIn(double t) const override;
+  void drawFadeOut(double t) const override;
+
+private:
+
+};
+
 #endif // !ADJUST_HPP
